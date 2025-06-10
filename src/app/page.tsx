ab5bd96946ad3e0b83ac519eb6a9d035b77d03bf
Axel Laurent <axosar@gmail.com>
2025-06-10 12:43:05 -0600

    entrada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-8xl font-extralight">WORLD CUP 2026</h1>
         <h3 className="text-2xl font-extralight text-center w-full uppercase">Canada - Mexico - USA</h3>
+        <h2 className="text-5xl font-extralight text-center w-full uppercase">11 JUN - 19 JUL</h2>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
