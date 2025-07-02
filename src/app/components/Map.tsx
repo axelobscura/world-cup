4b310e078637fbe5fd265e05de9f791704704f0b
Axel Laurent <axosar@gmail.com>
2025-07-01 19:22:28 -0600

    map

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useEffect } from 'react';
-import { MapContainer, Marker, Popup } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function Map() {
   useEffect(() => {
@@ -11,6 +11,10 @@ export default function Map() {
   }, []);
   return (
     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className='leaflet-container'>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
       <Marker position={[51.505, -0.09]}>
         <Popup>
           This is a popup
